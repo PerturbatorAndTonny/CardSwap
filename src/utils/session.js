e3956dc78d1fa9f1219ca6afaece6bad74b84665
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-21 10:55:15 -0500

    feat(session): implement blacklist for close session tokens

--- src/utils/session.js
+++ src/utils/session.js
@@ -2,12 +2,22 @@ import jwt from "jsonwebtoken"
 
 const key = process.env.JWT_KEY
 
+const blacklist = new Set([])
+
 export async function createSession(payload) {
-  const token = await jwt.sign(payload, "secretHere", { expiresIn: '1h' })
+  const token = await jwt.sign(payload, key, { expiresIn: '1h' })
   return token
 }
 
 export async function validateSession(token) {
-  const isSession = await jwt.verify(token, "secretHere")
+  const isSession = await jwt.verify(token, key)
   return isSession
+}
+
+export function addToBlackList (token){
+  blacklist.add(token)
+}
+
+export function isBlackListed(token) {
+  return blacklist.has(token)
 }
