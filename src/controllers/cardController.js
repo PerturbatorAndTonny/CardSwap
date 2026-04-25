c74335097790e9fa3021ffb4887b408fb455025c
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-24 22:26:30 -0500

    Enhancement(card): move functions for separate logic

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -1,22 +1,5 @@
 // oxlint-disable no-unused-vars
-import { Card } from "../models/cardModel.js"
-
-async function updateCardModel(id, reviewState, reason){ //la funcion es declarada para el modelo(simulado)
-    const updateCardReview = await Card.findByIdAndUpdate(
-        {_id: id},
-        { $set: {
-            reviewState: reviewState,
-            reason: reason || "No aplica",
-        }},
-        { new: true } 
-    )
-    return updateCardReview
-}
-
-async function saveCard_DB(cardData){
-    const newCard = await Card.create(cardData)
-    return newCard;
-}
+import { Card, saveCard, updateReviewCard } from "../models/cardModel.js"
 
 //Arrow function para el controlador (como pide el proyecto)
 export const patchCardReview = async (req, res) => {
@@ -24,7 +7,7 @@ export const patchCardReview = async (req, res) => {
     const {reviewState, reason} = req.body;
 
     try{
-        const updatedCard = await updateCardModel(id, reviewState, reason);
+        const updatedCard = await updateReviewCard(id, reviewState, reason);
 
         //respondemos con 201 segun el criterio que se tenga de aceptación
         res.status(201).json(updatedCard);
@@ -43,7 +26,7 @@ export const createdCard = async (req, res) =>{
         if(!traderExist){
             return res.status(400).json({message: "Usuario No Encontrado"});
         }
-        const newCard = await saveCard_DB({status, edition, language, idTrader});
+        const newCard = await saveCard({status, edition, language, idTrader});
 
         res.status(201).json(newCard);
     }catch(error){
