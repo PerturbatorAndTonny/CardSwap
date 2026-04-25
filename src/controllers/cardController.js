a60c2732ab8631ddd4c5ef2b88d10f76b1060ba2
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-24 22:27:40 -0500

    Enhancement(card): lint code for better standard

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -3,33 +3,33 @@ import { Card, saveCard, updateReviewCard } from "../models/cardModel.js"
 
 //Arrow function para el controlador (como pide el proyecto)
 export const patchCardReview = async (req, res) => {
-    const {id}= req.params;
-    const {reviewState, reason} = req.body;
+    const { id } = req.params;
+    const { reviewState, reason } = req.body;
 
-    try{
+    try {
         const updatedCard = await updateReviewCard(id, reviewState, reason);
 
         //respondemos con 201 segun el criterio que se tenga de aceptación
         res.status(201).json(updatedCard);
-    } catch(error){
-        res.status(500).json({message: "Error interno del servidor"});
+    } catch (error) {
+        res.status(500).json({ message: "Error interno del servidor" });
     }
 };
 
-export const createdCard = async (req, res) =>{
+export const createdCard = async (req, res) => {
 
-    try{ //criterio de aceptación donde se valida la existencia del trader
+    try { //criterio de aceptación donde se valida la existencia del trader
 
-        const { status, edition, language, idTrader} = req.body;
+        const { status, edition, language, idTrader } = req.body;
         const traderExist = true;
 
-        if(!traderExist){
-            return res.status(400).json({message: "Usuario No Encontrado"});
+        if (!traderExist) {
+            return res.status(400).json({ message: "Usuario No Encontrado" });
         }
-        const newCard = await saveCard({status, edition, language, idTrader});
+        const newCard = await saveCard({ status, edition, language, idTrader });
 
         res.status(201).json(newCard);
-    }catch(error){
-        res.status(500).json({message: "Error interno - Publicar Carta"});
+    } catch (error) {
+        res.status(500).json({ message: "Error interno - Publicar Carta" });
     }
 };
