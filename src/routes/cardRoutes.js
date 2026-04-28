9ad3a6b737d831d85f3d756bbc63fbea125d0352
santiago Torres <sntglopeztorres@gmail.com>
2026-04-28 13:55:55 -0500

    feat(card): create card routes structure

--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { verifyData } from "../middlewares/verifyData.js";
 import { createCardSchema, reviewCardSchema } from "../schemas/cardSchema.js";
-import { createdCard, patchCardReview } from "../controllers/cardController.js";
-
+import { createdCard, patchCardReview, getCards, getCard, editCard } from "../controllers/cardController.js";
+ 
 const router = Router();
-
+ 
 router.post("/card", verifyData(createCardSchema), createdCard);
-
+ 
+router.get("/card", getCards);
+ 
+router.get("/card/:id", getCard);
+ 
+router.put("/card/:id", verifyData(createCardSchema), editCard);
+ 
 router.patch("/card/:id", verifyData(reviewCardSchema), patchCardReview);
-
+ 
 export default router;
