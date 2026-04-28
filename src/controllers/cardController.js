6ae0740243a93bfd44cdcad679451308d7f8ef22
Mike <maquintinacero@gmail.com>
2026-04-28 16:17:22 -0500

    feat: implementation of a controller to delete publications

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -33,3 +33,40 @@ export const createdCard = async (req, res) => {
         res.status(500).json({ message: "Error interno - Publicar Carta" });
     }
 };
+
+//controlador para eliminar carta
+export const deleteCard = async (req, res) => {
+    const { id } = req.params;
+    const user = req.user;
+
+    try{
+        // Buscamos una carta primero para validar quien es el dueño
+        const card = await Card.findById(id);
+
+        if(!card){
+            return res.status(404).json({ message: "La carta NO existe"});
+        }
+
+        //valida permisos
+        //aqui se compara el idTrader de la carta con el id del usuario logueado
+        const isOwner = card.idTrader.toString() === user.id;
+        //verifica si el usuario es administrador
+        const isAdmin = user.role === 'admin';
+
+        if(!isOwner && !isAdmin){
+            return res.status(403).json({
+                message: "No tiene permisos para eliminar la publicación"
+            });
+        }
+
+        //si la validación se completa, se elimina la carta
+        await card.findByIdAndDelete(id);
+        res.status(500).json({message: "Publicación eliminada con exito."});
+
+
+    }catch(error){
+        console.error(error);
+
+        res.status(500).json({message: "Error interno - Eliminar Carta"})
+    }
+};
