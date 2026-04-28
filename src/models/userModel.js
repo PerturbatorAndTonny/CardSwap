7d0d41cdc30be45f1e682c785161c95df0d36c3d
Mike <maquintinacero@gmail.com>
2026-04-28 14:17:16 -0500

    feat: Creating the role field as an enum in MongoDB

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -29,8 +29,17 @@ function userSchema(){
                 required: true,
                 unique: true, // no puede haber dos usuarios con el mismo mail
                 trim: true, // convierte el mail a minúsculas automáticamente
-            }
+            },
+
+
+            //implementacion del campo role como un enum en MongoDB
+            role: {
+                type: String,      //el rol debe ser de tipo String
+                enum:["admin", "moderador", "usuario"],
+                default: "usuario"
+            }, description: {type: String, default: ""} //define el usuario de tipo default 'usuario'
         },
+
         { timestamps: true } // agrega createdAt y updatedAt automáticamente
     );
     // Retornamos el modelo con el nombre "User"
