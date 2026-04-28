7cbc82884dd9212f5cd3e4e99c6a4d68a79b8453
Mike <maquintinacero@gmail.com>
2026-04-28 16:24:24 -0500

    feat: update imports and add controller 'deleteCard' and route with verifyToken

--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { verifyData } from "../middlewares/verifyData.js";
 import { createCardSchema, reviewCardSchema } from "../schemas/cardSchema.js";
-import { createdCard, patchCardReview } from "../controllers/cardController.js";
+import { createdCard, patchCardReview, deleteCard } from "../controllers/cardController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post("/card", verifyData(createCardSchema), createdCard);
 
 router.patch("/card/:id", verifyData(reviewCardSchema), patchCardReview);
+
+router.delete("/card/:id", verifyToken, deleteCard);
 
 export default router;
