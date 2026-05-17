fe95fb37f61b2c47002947f28d75a808219f4229
Santiago T. <sntglopeztorres@gmail.com>
2026-05-16 20:16:41 -0500

    feat(ban): add base ban schema and model

---
+++ src/models/banModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+function banSchema() {
+const schema = new Schema(
+    {
+    idUser: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+
+    idAdmin: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+
+    reason: {
+        type: String,
+        required: true,
+        trim: true,
+        },
+    },
+    { timestamps: true }
+    );
+
+    return model("Ban", schema);
+}
+
+export const Ban = banSchema();
