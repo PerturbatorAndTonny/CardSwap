61ca579791aa2fe3879921869e621f6c60abc8af
Santiago T. <sntglopeztorres@gmail.com>
2026-05-16 20:47:27 -0500

    feat(admin): implement getReports controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,5 +1,8 @@
 import { User } from "../models/userModel.js";
 import { saveBan, findBanByUser } from "../models/banModel.js";
+import {
+    getAllReports,
+} from "../models/reportModel.js";
 
 export const banUser = async (req, res) => {
 try {
@@ -37,6 +40,26 @@ try {
     } catch (error) {
         res.status(500).json({
             message: "Error interno - Banear usuario",
+        });
+    }
+};
+// ─── HU6 - REVISAR REPORTES ───────────────────────────────────────────────────
+
+// GET /admin/reports → lista todos los reportes
+export const getReports = async (req, res) => {
+try {
+    const reports = await getAllReports();
+
+    if (!reports || reports.length === 0) {
+        return res.status(404).json({
+            message: "No hay reportes registrados",
+        });
+    }
+
+    res.status(200).json(reports);
+    } catch (error) {
+        res.status(500).json({
+        message: "Error interno - Listar reportes",
         });
     }
 };
