5d35a5e64944592367aa047db751c7164b6393a3
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-05-17 14:22:38 -0500

    feat(offer): add route for accepet and confirm offer delivery

--- src/routes/offerRoutes.js
+++ src/routes/offerRoutes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { verifyData } from "../middlewares/verifyData.js";
 import { verifyToken, verifyRole } from "../middlewares/verifySession.js";
-import { createOffer, getOffers } from "../controllers/offerController.js"
+import { createOffer, getOffers, acceptOffer, confirmDelivery } from "../controllers/offerController.js"
 
 import { offerSchema } from '../schemas/offerScheme.js'
 
 const router = Router();
 
-router.post("/offer", verifyData(offerSchema), verifyToken, /*verifyRole("admin"),*/ createOffer);
+router.post("/offer", verifyData(offerSchema), verifyToken, verifyRole("admin"), createOffer);
 
 router.get("/offer", verifyToken, verifyRole("admin"), getOffers);
+
+router.put("/offer/:idOffer/accept", verifyToken, verifyRole("admin"), acceptOffer);
+
+router.put("/offer/:idOffer/confirm", verifyToken, verifyRole("admin"), confirmDelivery);
 
 export default router;
